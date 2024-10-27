d77351e8bd90e97b3df94f09c332c0401ce5865e
AlexKhabibov <alex.lv.u@gmail.com>
2024-10-27 16:29:30 +0300

    react HW4

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
